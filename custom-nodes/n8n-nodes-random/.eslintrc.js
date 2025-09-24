6029410b56ea460514bbd430b32cc8b3a2b39057
Arthur Patrocinio Neves <arthur.patrocinio-neves@epitech.eu>
2025-09-23 22:12:35 -0300

    feat: INICIAL custom node
    Package.json
    
    TypeScript configurado para desenvolvimento
    
    Estrutura base

---
+++ custom-nodes/n8n-nodes-random/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: './tsconfig.json',
+		sourceType: 'module',
+		extraFileExtensions: ['.json']
+	},
+	plugins: [
+		'@typescript-eslint',
+		'n8n-nodes-base',
+	],
+	extends: [
+		'eslint:recommended',
+		'@typescript-eslint/recommended',
+		'plugin:n8n-nodes-base/community',
+	],
+	rules: {
+		'n8n-nodes-base/community-package-json-name-still-default': 'off',
+	},
+};
